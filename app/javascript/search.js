6850e2ac06a88315b54124ab48cf790751a7fc39
nakayu08 <nakabo.yu08@gmail.com>
2023-09-23 18:07:27 +0900

    表示機能の実装

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,41 +1,28 @@
 function search (){
-  const searchText  = document.getElementById("search-text");
-  var targetText=[];
-
-  searchText.addEventListener("keyup", () => {
-    //console.log(searchText.value);
-    var searchResult;
- 
-    // 検索結果を格納するための配列を用意
-    searchResult = [];
+  var $searchInput = document.getElementById("search-text"); // 入力エリア
+  const targetText=document.getElementById("recipe_list")
+  const $searchElem = targetText.getElementsByTagName('li');
+  console.log($searchInput.value)
   
-    // 検索結果エリアの表示を空にする
-    $('#search-result__list').empty();
+  var excludedClass ='is-excluded'; // 絞り込み対象外の要素に付与するclass
 
-    // 検索ボックスに値が入ってる場合
-    if (searchText != '') {
-        if (targetText.indexOf(searchText) != -1) {
-          // 存在する場合はそのリストのテキストを用意した配列に格納
-          searchResult.push(targetText);
+
+  // 絞り込み処理
+  $searchInput.addEventListener("keyup", () => {
+    if ($searchInput != '') {
+      var keywordArr = $searchInput.value
+      console.log(keywordArr)
+
+      for (var i = 0; i < $searchElem.length; i++) {
+        $searchElem[i].style.display ="block"; // 現在非表示にしているリストを表示する
+
+        var thisString = $searchElem[i].textContent.toString();
+        if(thisString.indexOf(keywordArr) == -1) { // 入力文字列と一致する文字列がない場合
+          $searchElem[i].style.display ="none"
         }
       }
-      // 検索結果をページに出力
-      for (var i = 0; i < searchResult.length; i ++) {
-        $('<span>').text(searchResult[i]).appendTo('#search-result__list');
-      }
-    });
-};
-
-  const recipeurl = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&categoryType=small&elements=categoryName%2CcategoryUrl&applicationId=1022110364284411341';
-  $.getJSON(recipeurl, (data) => {
-    const recipes = data.result;
-    updateText(recipes);
-  });
-  const updateText = (data) => {
-    for (let i = 0; i < data.small.length; i++) {
-      //console(data.small[i].categoryName)
-      targetText.push(data.small[i].categoryName);
     }
-  }
+  });
+};
 
 window.addEventListener('load', search);
