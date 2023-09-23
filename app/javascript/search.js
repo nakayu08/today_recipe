c1679f1b79bc4cb5f6738173f6af6b5cbb8e0cb5
nakayu08 <nakabo.yu08@gmail.com>
2023-09-23 19:06:09 +0900

    コメントの削除

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -2,16 +2,11 @@ function search (){
   var $searchInput = document.getElementById("search-text"); // 入力エリア
   const targetText=document.getElementById("recipe_list")
   const $searchElem = targetText.getElementsByTagName('li');
-  console.log($searchInput.value)
-  
-  var excludedClass ='is-excluded'; // 絞り込み対象外の要素に付与するclass
-
 
   // 絞り込み処理
   $searchInput.addEventListener("keyup", () => {
     if ($searchInput != '') {
       var keywordArr = $searchInput.value
-      console.log(keywordArr)
 
       for (var i = 0; i < $searchElem.length; i++) {
         $searchElem[i].style.display ="block"; // 現在非表示にしているリストを表示する
