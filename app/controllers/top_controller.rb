8143ab51d050c002166405ad984b625da81c6dc4
nakayu08 <nakabo.yu08@gmail.com>
2023-09-24 17:21:32 +0900

    コントローラーの修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,4 +1,16 @@
 class TopController < ApplicationController
   def index
+    @categoryName = params[:categoryName]
+    @categoryId= params[:categoryId]
+    @parentCategoryId= params[:parentCategoryId]
+    @categoryUrl= params[:categoryUrl]
   end
+
+  def show
+    @categoryName = params[:categoryName]
+    @categoryId= params[:categoryId]
+    @parentCategoryId= params[:parentCategoryId]
+    @categoryUrl= params[:categoryUrl]
+  end
+
 end
