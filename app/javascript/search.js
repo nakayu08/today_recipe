db966befabac0936a433f202dae9abaf96dfe032
nakayu08 <nakabo.yu08@gmail.com>
2023-09-24 16:19:43 +0900

    カタカナ、ひらがなの区別をなくす。

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -8,11 +8,22 @@ function search (){
     if ($searchInput != '') {
       var keywordArr = $searchInput.value
 
+      var keywordkanaAr;
+      var keywordhiraAr;
+      
+      keywordkanaAr = hiraToKana(keywordArr); //カタカナへの変換
+      keywordhiraAr = kanaToHira(keywordArr); //ひらがなへの変換
+      
+
+     //console.log(keywordArr)
+     //console.log(keywordkanaAr)
+     //console.log(keywordhiraAr)
+
       for (var i = 0; i < $searchElem.length; i++) {
-        $searchElem[i].style.display ="block"; // 現在非表示にしているリストを表示する
+        $searchElem[i].style.display =""; // 現在非表示にしているリストを表示する
 
         var thisString = $searchElem[i].textContent.toString();
-        if(thisString.indexOf(keywordArr) == -1) { // 入力文字列と一致する文字列がない場合
+        if(thisString.indexOf(keywordkanaAr) == -1 && thisString.indexOf(keywordhiraAr)== -1) { // 入力文字列と一致する文字列がない場合
           $searchElem[i].style.display ="none"
         }
       }
@@ -21,3 +32,17 @@ function search (){
 };
 
 window.addEventListener('load', search);
+
+function kanaToHira(str) {
+  return str.replace(/[\u30a1-\u30f6]/g, function(match) {
+      var chr = match.charCodeAt(0) - 0x60;
+      return String.fromCharCode(chr);
+  });
+}
+
+function hiraToKana(str) {
+  return str.replace(/[\u3041-\u3096]/g, function(match) {
+      var chr = match.charCodeAt(0) + 0x60;
+      return String.fromCharCode(chr);
+  });
+}
