a96654ad87e83a78162d0026825e219caf414d48
nakayu08 <nakabo.yu08@gmail.com>
2023-09-24 16:42:19 +0900

    APIのURLの修正

--- app/javascript/recipe.js
+++ app/javascript/recipe.js
@@ -1,6 +1,8 @@
 // 生成したURL
 //const url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=1022110364284411341';
-const recipeurl = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&categoryType=small&elements=categoryName%2CcategoryUrl&applicationId=1022110364284411341';
+//const recipeurl = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&categoryType=small&elements=categoryName%2CcategoryUrl&applicationId=1022110364284411341';
+
+const recipeurl = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?format=json&categoryType=medium&applicationId=1022110364284411341';
 
 
 // 書き出し
@@ -21,10 +23,10 @@ const recipeurl = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20
 
 const updateText = (data) => {
   //console.log(data.small[20]);
-  for (let i = 0; i < data.small.length; i++) {
+  for (let i = 0; i < data.medium.length; i++) {
       const insertHtml = `
           <li class="js-extraction">
-            <a id="recipeurl" href='${data.small[i].categoryUrl}'>${data.small[i].categoryName}</a>
+            <a id="recipeurl" href='${data.medium[i].categoryUrl}'>${data.medium[i].categoryName}</a>
           </li>
       `;
       $('#recipe_list').append(insertHtml);
