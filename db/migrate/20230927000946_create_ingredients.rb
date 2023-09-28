6a6a856ad6ebbaaa68246ad016eb1e31a2dc1e5a
nakayu08 <nakabo.yu08@gmail.com>
2023-09-28 15:55:54 +0900

    カラムの追加

--- db/migrate/20230927000946_create_ingredients.rb
+++ db/migrate/20230927000946_create_ingredients.rb
@@ -1,7 +1,10 @@
 class CreateIngredients < ActiveRecord::Migration[6.0]
   def change
     create_table :ingredients do |t|
-
+      t.string :name, null: false
+      t.integer :categoryId, null: false
+      t.integer :parentCategoryId, null: false
+      t.string :categoryUrl, null: false
       t.timestamps
     end
   end
