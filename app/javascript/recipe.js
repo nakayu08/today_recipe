f05143daf25a215e78ee89f81184a2532c00ca1f
nakayu08 <nakabo.yu08@gmail.com>
2023-09-30 17:11:56 +0900

    aタグのメソッドの指定

--- app/javascript/recipe.js
+++ app/javascript/recipe.js
@@ -22,16 +22,20 @@ const recipeurl = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20
 //}
 //<a id="recipeurl" href='${data.medium[i].categoryUrl}'>${data.medium[i].categoryName}</a>
 //<%=link_to ${data.medium[i].categoryName},top_path(id:${data.medium[i].parentCategoryId+'-'+data.medium[i].categoryId} , categoryId:${data.medium[i].categoryId} ,categoryName:${data.medium[i].categoryName},categoryUrl:${data.medium[i].categoryUrl},parentCategoryId:${data.medium[i].parentCategoryId})%>
+//<a id="recipeurl" href=top/${data.medium[i].categoryId} ,categoryId: ${data.medium[i].categoryId} ,categoryName: ${data.medium[i].categoryName},categoryUrl: ${data.medium[i].categoryUrl},parentCategoryId: ${data.medium[i].parentCategoryId}>${data.medium[i].categoryName}</a>
+//<a id="recipeurl" href=top/${data.medium[i].categoryId}  :categoryId=> ${data.medium[i].categoryId} ,:categoryName=> ${data.medium[i].categoryName},:categoryUrl=> ${data.medium[i].categoryUrl},:parentCategoryId=> ${data.medium[i].parentCategoryId}</a>
+//<%= link_to ${data.medium[i].categoryName}, ingredient_index_path(:categoryId=> ${data.medium[i].categoryId} ,:name=> ${data.medium[i].categoryName},:categoryUrl=> ${data.medium[i].categoryUrl},:parentCategoryId=> ${data.medium[i].parentCategoryId}), method: :post %>
 
 const updateText = (data) => {
   //console.log(data.small[20]);
-  for (let i = 0; i < data.medium.length; i++) {
+  for (let i = 0; i < 5; i++) {
       const insertHtml = `
           <li class="js-extraction">
-            <a id="recipeurl" href=top/${data.medium[i].categoryId} ,categoryId: params[:${data.medium[i].categoryId}] ,categoryName: params[:${data.medium[i].categoryName}],categoryUrl: params[:${data.medium[i].categoryUrl}],parentCategoryId: params[:${data.medium[i].parentCategoryId}])>${data.medium[i].categoryName}</a>
+            <a id ="recipeurl" href='/ingredient' data-method="post">${data.medium[i].categoryName}</a>
           </li>
       `;
       $('#recipe_list').append(insertHtml);
+      console.log(insertHtml)
   }
 }
 
