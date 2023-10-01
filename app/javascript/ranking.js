e1db8434382f68f5fb37052ca04cd174c08b257a
nakayu08 <nakabo.yu08@gmail.com>
2023-10-01 16:26:47 +0900

    食材ごとのランキング上位のレシピ表示

--- app/javascript/ranking.js
+++ app/javascript/ranking.js
@@ -1,10 +1,22 @@
-const url = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?applicationId=[アプリID]';
 
-// 書き出し
+
+window.onload = function(){
+  var ele = document.getElementById('categoryId'); // HTML要素オブジェクトを取得
+  var urljoin = 'https://app.rakuten.co.jp/services/api/Recipe/CategoryRanking/20170426?format=json&categoryId='+ele.textContent +'&applicationId=1022110364284411341';
+  //console.log(urljoin);
+
+  // API取得
+  $.getJSON(urljoin, (data) => {
+    const recipes = data.result;
+    updateText(recipes);
+  });
+}
+  
+
 const updateText = (data) => {
     for (let i = 0; i < data.length; i++) {
         const insertHtml = `
-            <li>
+            <li class="ranking_li">
                 <a href="${data[i].recipeUrl}" target="_blank">
                     <img src="${data[i].foodImageUrl}" alt="${data[i].recipeTitle} 画像">
                 </a>
@@ -16,8 +28,5 @@ const updateText = (data) => {
     }
 }
 
-// API取得
-$.getJSON(url, (data) => {
-    const recipes = data.result;
-    updateText(recipes);
-});
+
+
