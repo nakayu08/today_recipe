95659c6a2599203ed553345f58adb084bcdd9bf7
nakayu08 <nakabo.yu08@gmail.com>
2023-10-03 11:08:59 +0900

    カラムを追加

--- db/migrate/20231003020750_create_favorites.rb
+++ db/migrate/20231003020750_create_favorites.rb
@@ -1,6 +1,8 @@
 class CreateFavorites < ActiveRecord::Migration[6.0]
   def change
     create_table :favorites do |t|
+      t.references :user, foreign_key: true
+      t.references :ingredient, foreign_key: true
 
       t.timestamps
     end
