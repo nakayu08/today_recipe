0e80da255fbccc235b74c859ef68f59feed04c12
nakayu08 <nakabo.yu08@gmail.com>
2023-10-03 11:16:10 +0900

    メソッドを定義

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,8 @@ class Ingredient < ApplicationRecord
   def full_category
     self.parentCategoryId.to_s + "-" + self.categoryId.to_s
   end
+
+  def favorites_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
 end
