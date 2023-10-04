45813655b503f572435e1f85a9d1c5fad29fe1b0
nakayu08 <nakabo.yu08@gmail.com>
2023-10-04 10:29:06 +0900

    お気に入りリスト表示のための変数追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
     @nickname = current_user.nickname
+
+    favorites = Favorite.where(user_id: current_user.id).pluck(:ingredient_id)  # ログイン中のユーザーのお気に入りのingredientカラムを取得
+    @favorite_list =Ingredient.find(favorites)
   end
 end
