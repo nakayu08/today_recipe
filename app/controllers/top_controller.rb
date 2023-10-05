d630254ce7afed2ba2c0a6677c74674e128c140b
nakayu08 <nakabo.yu08@gmail.com>
2023-10-05 17:10:41 +0900

    いらない記述の削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,16 +1,9 @@
 class TopController < ApplicationController
   def index
-    @categoryName = params[:categoryName]
-    @categoryId= params[:categoryId]
-    @parentCategoryId= params[:parentCategoryId]
-    @categoryUrl= params[:categoryUrl]
+   
   end
 
   def show
-    @categoryName = params[:categoryName]
-    @categoryId= params[:categoryId]
-    @parentCategoryId= params[:parentCategoryId]
-    @categoryUrl= params[:categoryUrl]
   end
 
 end
