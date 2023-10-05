89bf97d1ebd85f14b1efb1b957fa71b82dea5d08
nakayu08 <nakabo.yu08@gmail.com>
2023-10-05 17:07:54 +0900

    他のユーザーからのアクセスを制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,19 @@
 class UsersController < ApplicationController
-  def show
-    @nickname = current_user.nickname
+  before_action :is_matching_login_user
 
-    favorites = Favorite.where(user_id: current_user.id).pluck(:ingredient_id)  # ログイン中のユーザーのお気に入りのingredientカラムを取得
-    @favorite_list =Ingredient.find(favorites)
+  def show
+      @nickname = current_user.nickname
+
+      favorites = Favorite.where(user_id: current_user.id).pluck(:ingredient_id)  # ログイン中のユーザーのお気に入りのingredientカラムを取得
+      @favorite_list =Ingredient.find(favorites)
+  end
+
+  private
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to root_path
+    end
   end
 end
