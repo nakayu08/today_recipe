f73009c460fad55d5f067226004e3d09eaa19229
nakayu08 <nakabo.yu08@gmail.com>
2023-10-05 17:08:17 +0900

    ログインしているかのチェック

--- app/controllers/ingredient_controller.rb
+++ app/controllers/ingredient_controller.rb
@@ -1,4 +1,6 @@
 class IngredientController < ApplicationController
+  before_action :move_to_top, except: [:create, :show]
+
   def create
     @ingredient=Ingredient.new(ingredient_params)
     if @ingredient.save
@@ -16,5 +18,11 @@ class IngredientController < ApplicationController
   private
     def ingredient_params
         params.permit(:name,:categoryId , :categoryUrl,:parentCategoryId)
+    end
+
+    def move_to_top
+      unless user_signed_in?
+        redirect_to root_path
+      end
     end
 end
