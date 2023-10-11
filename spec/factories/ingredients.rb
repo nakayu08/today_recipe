48e04e834994fedbb1cf4d0e919b58a17bda07db
nakayu08 <nakabo.yu08@gmail.com>
2023-10-11 09:05:07 +0900

    fakerの作成

--- spec/factories/ingredients.rb
+++ spec/factories/ingredients.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :ingredient do
-    name {Faker::Lorem.sentence}
+    name {Faker::Lorem.word}
     categoryId {Faker::Number.number(3)}
     parentCategoryId {Faker::Number.number(2)}
     categoryUrl {Faker::Internet.url }
